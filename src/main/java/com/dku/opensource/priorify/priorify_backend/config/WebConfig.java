b58007e32914192e9e4b3579028d306f769d8338
JunSeo99 <an001209@gmail.com>
2025-05-26 03:06:55 +0900

    [FIX] local host Cors 추가

--- src/main/java/com/dku/opensource/priorify/priorify_backend/config/WebConfig.java
+++ src/main/java/com/dku/opensource/priorify/priorify_backend/config/WebConfig.java
@@ -19,8 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("https://priorify-one.vercel.app")
+                .allowedOrigins("http://localhost:3000", "https://priorify-one.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
