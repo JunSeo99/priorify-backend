a34c6a27163b009fe9ba435f4f56a454f66fcb43
JunSeo99 <an001209@gmail.com>
2025-05-26 11:32:22 +0900

    [FIX] deserialize 문제 해결

--- src/main/java/com/dku/opensource/priorify/priorify_backend/dto/PriorityDto.java
+++ src/main/java/com/dku/opensource/priorify/priorify_backend/dto/PriorityDto.java
@@ -5,10 +5,14 @@ import java.util.List;
 import com.dku.opensource.priorify.priorify_backend.model.CategoryPriority;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PriorityDto {
     private List<CategoryPriority> highPriorities;
     private List<CategoryPriority> lowPriorities;
